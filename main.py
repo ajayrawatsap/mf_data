917b4b0421e12982041eb2ceba7262bef827a952
ajaykgp12@gmail.com <ajaykgp12@gmail.com>
2021-07-03 11:57:11 +0530

    argument parsing

--- main.py
+++ main.py
@@ -5,14 +5,24 @@ import src.constants as ct
 from src.parsedata import DataParser
 from src.initialize import Logger
 from src.capitalgains import CapitalGains
+import argparse
 
-def main(file_name:str, display_logs ='yes', target_ltcg:float = 100000.00 ):
-
+# def main(file_name:str, display_logs ='yes', target_ltcg:float = 100000.00 ):
+def main(display_logs ='yes'):
+  
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_name", help="Name of text file stored in data directory", type = str)
+    parser.add_argument("target_ltcg",  nargs='?',   default = 100000, help="Target LTCG amount that you want tax free:Default INR 100000", type = int )
+    args = parser.parse_args()
+    
+    file_name = args.file_name
+    target_ltcg = args.target_ltcg
 
     logger = Logger()
     logger.initilaze_logging(display_logs)    
 
     logging.info(f'{"*" * 50}Start Of Process{"*" * 50}')
+    logging.info(f'File Name  {file_name}, Target LTCG is { target_ltcg}')
 
     dp = DataParser()
     file_path = os.path.join(ct.DATA_DIR, file_name)
@@ -25,5 +35,5 @@ def main(file_name:str, display_logs ='yes', target_ltcg:float = 100000.00 ):
     logging.info(f'{"*" * 50}End Of Process{"*" * 50}\n') 
 
 if __name__ == "__main__":
-    main('sample_data.txt')
+    main()
 
